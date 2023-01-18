e79ce1d87ec956dfb8cfff3ba826c97bb9ccef06
vishalrathore8oct <vishal.rathore8oct@gmail.com>
2023-01-18 16:26:58 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,108 @@
+print("Hello vishal sir!")
+#--------------------------------------------------------------
+a = int(input("Enter the value of a: "))
+b = int(input("Enter the value of b: "))
+c = int(input("Enter the value of c: "))
+d = int(input("Enter the value of d: "))
+e = int(input("Enter the value of e: "))
+f = int(input("Enter the value of f: "))
+#--------------------------------------------------------------
+if a>0 and c>0 and e>0:
+  if (a-e)>=0 and (a-c)>=0 and (a-e)<10 and (a-c)<10:
+    print("your variables (a-e,a-c) outputs is correct ")
+    print(a-e,a-c)
+  else:
+    print("your variables (a-e,a-c) outputs is wrong")
+  #   print(a-e,a-c)
+else:
+    print("your variables (a-e,a-c) outputs is wrong")
+#--------------------------------------------------------------
+if b>0 and c>0 and f>0:
+  if (c-f)>=0 and (b-c)>=0 and (c-f)<10 and (b-c)<10:
+    print("your variables (c-f,b-c) outputs is correct ")
+    print(c-f,b-c)
+  else:
+    print("your variables (c-f,b-c) outputs is wrong")
+  #   print(c-f,b-c)
+else:
+    print("your variables (c-f,b-c) outputs is wrong")
+#--------------------------------------------------------------
+if b>0 and c>0 and f>0:
+  if (c-f)>=0 and (b-f)>=0 and (c-f)<10 and (b-f)<10:
+    print("your variables (c-f,b-f) outputs is correct ")
+    print(c-f,b-f)
+  else:
+    print("your variables (c-f,b-f) outputs is wrong")
+  #   print(c-f,b-f)
+else:
+    print("your variables (c-f,b-f) outputs is wrong")
+#--------------------------------------------------------------
+if b>0 and c>0 and e>0:
+  if (b-c)>=0 and (b-e)>=0 and (b-c)<10 and (b-e)<10:
+    print("your variables (b-c,b-e) outputs is correct ")
+    print(b-c,b-e)
+  else:
+    print("your variables (b-c,b-e) outputs is wrong")
+  #   print(b-c,b-e)
+else:
+    print("your variables (b-c,b-e) outputs is wrong")
+#--------------------------------------------------------------
+if b>0 and c>0 and e>0:
+  if (c-b)>=0 and (c-e)>=0 and (c-b)<10 and (c-e)<10:
+    print("your variables (c-b,c-e) outputs is correct ")
+    print(c-b,c-e)
+  else:
+    print("your variables (c-b,c-e) outputs is wrong")
+  #   print(c-b,c-e)
+else:
+    print("your variables (c-b,c-e) outputs is wrong")
+#--------------------------------------------------------------
+if a>0 and b>0 and e>0:
+  if (a+e)>=0 and (a-b)>=0 and (a+e)<10 and (a-b)<10:
+    print("your variables (a+e,a-b) outputs is correct ")
+    print(a+e,a-b)
+  else:
+    print("your variables (a+e,a-b) outputs is wrong")
+  #   print(a+e,a-b)
+else:
+    print("your variables (a+e,a-b) outputs is wrong")
+#--------------------------------------------------------------
+if a>0 and b>0 and e>0:
+  if (b+e)>=0 and (a-b)>=0 and (b+e)<10 and (a-b)<10:
+    print("your variables (b+e,a-b) outputs is correct ")
+    print(b+e,a-b)
+  else:
+    print("your variables (b+e,a-b) outputs is wrong")
+  #   print(b+e,a-b)
+else:
+    print("your variables (b+e,a-b) outputs is wrong")
+#--------------------------------------------------------------
+if a>0 and b>0 and e>0 and f>0:
+  if (e+f)>=0 and (a-b)>=0 and (e+f)<10 and (a-b)<10:
+    print("your variables (e+f,a-b) outputs is correct ")
+    print(e+f,a-b)
+  else:
+    print("your variables (e+f,a-b) outputs is wrong")
+  #   print(e+f,a-b)
+else:
+    print("your variables (e+f,a-b) outputs is wrong")
+#--------------------------------------------------------------
+if a>0 and b>0 and e>0 and f>0:
+  if (b-f)>=0 and (a+e)>=0 and (b-f)<10 and (a+e)<10:
+    print("your variables (b-f,a+e) outputs is correct ")
+    print(b-f,a+e)
+  else:
+    print("your variables (b-f,a+e) outputs is wrong")
+  #   print(b-f,a+e)
+else:
+    print("your variables (b-f,a+e) outputs is wrong")
+#--------------------------------------------------------------
+if a>0 and b>0 and d>0 and e>0:
+  if (e-a)>=0 and (b+d)>=0 and (e-a)<10 and (b+d)<10:
+    print("your variables (e-a,b+d) outputs is correct ")
+    print(e-a,b+d)
+  else:
+    print("your variables (e-a,b+d) outputs is wrong")
+  #   print(e-a,b+d)
+else:
+    print("your variables (e-a,b+d) outputs is wrong")
